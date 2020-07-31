8508a5bfa80ef795a33d4ff690f843b5e2874f70
Vladislav Sorokin <sorokinvj@gmail.com>
2020-07-31 16:00:57 +0100

    bsic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const APPStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`
 interface IApp {}
 const App: React.FC<IApp> = () => {
-  return <div className="App">There will be weather app</div>
+  return <APPStyled>There will be weather app</APPStyled>
 }
 
 export default App
