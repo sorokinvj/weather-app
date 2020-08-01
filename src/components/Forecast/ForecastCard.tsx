06a2e238d6af6348cf783db1d403ea0697746a4c
Vladislav Sorokin <sorokinvj@gmail.com>
2020-08-01 18:52:06 +0100

    check condition in Forecast

--- src/components/Forecast/ForecastCard.tsx
+++ src/components/Forecast/ForecastCard.tsx
@@ -9,18 +9,20 @@ interface IForecastCard {
 }
 
 const ForecastCard: React.FC<IForecastCard> = ({ title, weather }) => {
-  return (
-    <ForecastCardStyled>
-      <h3 className="day">{title}</h3>
-      <h3 className="date">{moment(weather!.dt_txt).format('D MMM')}</h3>
-      <img
-        className="icon"
-        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-        alt={`${weather.weather[0].description} icon`}
-      />
-      <h2 className="temp">{Math.round(weather!.main.feels_like)} &#176;C</h2>
-    </ForecastCardStyled>
-  )
+  if (weather) {
+    return (
+      <ForecastCardStyled>
+        <h3 className="day">{title}</h3>
+        <h3 className="date">{moment(weather!.dt_txt).format('D MMM')}</h3>
+        <img
+          className="icon"
+          src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+          alt={`${weather.weather[0].description} icon`}
+        />
+        <h2 className="temp">{Math.round(weather!.main.feels_like)} &#176;C</h2>
+      </ForecastCardStyled>
+    )
+  } else return null
 }
 
 export default ForecastCard
