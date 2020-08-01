dd6ca2376ce7d15122331bef2291974ab413a847
Vladislav Sorokin <sorokinvj@gmail.com>
2020-08-02 00:26:26 +0100

    gradient for background

--- src/components/App/globalStyles.ts
+++ src/components/App/globalStyles.ts
@@ -1,13 +1,11 @@
 import { createGlobalStyle } from 'styled-components/macro'
-import sky from './sky.jpg'
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     font-family: 'Open Sans', sans-serif;  
-    background: url(${sky});
-    background-size: cover;
+    background: linear-gradient(180deg, #8BABCB 0%, #BBC0C3 39.72%, #DFD2BB 69.27%, #FFCB80 100%);    
     min-height: 100vh;
   }
 `
