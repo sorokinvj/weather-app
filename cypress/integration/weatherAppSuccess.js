eb375151deb49154c6d4f4273962712a7ad75e41
Vladislav Sorokin <sorokinvj@gmail.com>
2020-08-02 02:25:38 +0100

    error test

--- cypress/integration/weatherAppSuccess.js
+++ cypress/integration/weatherAppSuccess.js
@@ -1,4 +1,4 @@
-describe('Weather app', () => {
+describe('Weather app succesful', () => {
   it('clicks suggest option and gets forecast', () => {
     cy.visit('/')
     cy.findByAltText('input city').should('exist')
@@ -19,5 +19,22 @@ describe('Weather app', () => {
     cy.findByAltText('input city').click().type('Manchester')
     cy.get('.city-option:first').click()
     cy.get('.forecast-city').should('have.length', 2)
+  })
+})
+
+describe('Weather app Errors', () => {
+  it('not found city in db', () => {
+    cy.visit('/')
+    cy.findByAltText('input city').click().type('dkjksdkjshksdjhsd')
+    cy.findByText(/Search/).click()
+    cy.get('.error-message')
+      .should('exist')
+      .contains("Sorry, we don't have this city in our database. Maybe it doesn't exist?")
+  })
+  it('bad connection error', () => {
+    cy.server({ force404: true })
+    cy.visit('/')
+    cy.findByAltText('input city').click().type('london')
+    cy.get('.city-option:first').click()
   })
 })
